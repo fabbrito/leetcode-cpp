b113a3f71f3379b1dba0ffd2251c877574d3b7d9
Fabrício <fab.brito94@gmail.com>
2023-11-29 16:06:29 -0300

    Remove some spaces

--- Utils/src/printMatrix.h
+++ Utils/src/printMatrix.h
@@ -5,13 +5,13 @@
 namespace utils {
 	template <typename T>
 	void printMatrix(const std::vector<std::vector<T>>& vec, bool flat = false, std::string separator = ", ") {
-		std::cout << (flat ? "{ " : "{\r\n");
+		std::cout << (flat ? "{" : "{\r\n");
 		for (int outer_i = 0; outer_i < vec.size(); outer_i++) {
 			if (!flat) std::cout << "  ";
 			printVector(vec[outer_i], separator, false);
 			if (outer_i < vec.size() - 1)
 				std::cout << separator;
-			std::cout << (flat ? " " : "\r\n");
+			std::cout << (flat ? "" : "\r\n");
 		}
 		std::cout << (flat ? "}" : "}\r\n");
 	}
--- Utils/src/printVector.h
+++ Utils/src/printVector.h
@@ -6,12 +6,12 @@
 namespace utils {
 	template <typename T>
 	void printVector(const std::vector<T>& vec, std::string separator = ", ", bool endLine = false) {
-		std::cout << "{ ";
+		std::cout << "{";
 		for (std::size_t i = 0; i < vec.size(); i++) {
 			if (i < vec.size() - 1) std::cout << vec[i] << separator;
 			else std::cout << vec[i];
 		}
-		std::cout << " }";
+		std::cout << "}";
 		if (endLine) {
 			std::cout << std::endl;
 		}
